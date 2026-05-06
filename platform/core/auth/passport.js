1e0754db62efaafa59aeef0e6289b2273bd5ea91
= <=>
2026-05-06 07:10:16 +0000

    fixed error with google auth

--- platform/core/auth/passport.js
+++ platform/core/auth/passport.js
@@ -4,7 +4,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID || 'dummy_client_id',
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || 'dummy_client_secret',
-    callbackURL: '/auth/google/callback',
+    callbackURL: `${process.env.API_BASE_URL || 'http://localhost:3000'}/auth/google/callback`,
     passReqToCallback: true 
   },
   function(req, accessToken, refreshToken, profile, cb) {
